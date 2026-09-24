5a477ffb5c7f328680b8bbe4d172338021684104
Lachlan Whyborn <lachlan.whyborn@anu.edu.au>
2026-09-24 14:47:23 +1000

    Add landfrac support (#43)
    * Add masks from land fractions
    
    * Remove extra ws
    
    * Add task definition and minor tweaks to script
    
    * Fix typos
    
    * Fix bug in landfrac script
    
    * Copy land fractions to working dir
    
    * Fix script choice for land fractions

---
+++ app/landseamask_from_land_fractions/file/landseamask_from_land_fractions.py
@@ -0,0 +1,67 @@
+import argparse
+import ants
+import numpy
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '--land-fractions',
+            required=True,
+            help='Input land fractions used to create masks.'
+            )
+
+    parser.add_argument(
+            '--output-path',
+            required=True,
+            help='Where to write generated land and sea masks.'
+            )
+
+    return parser.parse_args()
+
+
+def landfrac_to_landmask(landfrac_path, out_path):
+    land_fracs = ants.io.load.load_cube(landfrac_path)
+
+    # We'll also want to copy the land fractions to the working dir
+    ants.io.save.netcdf(land_fracs, out_path + '/qrparm.landfrac')
+    ants.io.save.ancil(land_fracs, out_path + '/qrparm.landfrac')
+
+    # Don't worry about thresholding- assume user has already done desired
+    # preprocessing of removing almost-0 landfracs
+    landmask = land_fracs.copy(data=(land_fracs.data > 0.0).astype(numpy.int64))
+    landmask.attributes["valid_min"] = 0
+    landmask.attributes["valid_max"] = 1
+    landmask.attributes["grid_staggering"] = 6
+    landmask.attributes["name"] = 'land mask'
+    landmask.attributes["STASH"] = 'm01s00i30'
+
+    for dim in landmask.coords():
+        dim.bounds = None
+
+    ants.io.save.netcdf(landmask, out_path + '/qrparm.mask')
+    ants.io.save.ancil(landmask, out_path + '/qrparm.mask')
+
+    # The land mask and sea mask are not inversions of each other- they overlap
+    # in grid cells when 0.0 < fraction < 1.0
+    seamask = land_fracs.copy(data=(land_fracs.data < 1.0).astype(numpy.int64))
+    seamask.attributes["valid_min"] = 0
+    seamask.attributes["valid_max"] = 1
+    seamask.attributes["grid_staggering"] = 6
+    seamask.attributes["name"] = 'sea mask'
+    seamask.attributes["STASH"] = 'm01s00i30'
+
+    for dim in seamask.coords():
+        dim.bounds = None
+
+    ants.io.save.netcdf(seamask, out_path + '/qrparm.mask_sea')
+    ants.io.save.ancil(seamask, out_path + '/qrparm.mask_sea')
+
+
+if __name__ == '__main__':
+    args = _parse_args()
+    
+    landfrac_to_landmask(
+            args.land_fractions,
+            args.output_path,
+            )
